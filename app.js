b25b72d43a3a73f0b3d4abb8f0df71a52c61555a
Travis Haley <thaley@dlr360.com>
2017-05-05 10:55:11 -0600

    removehead and removetail implimented

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 var ll = require('./linkedlist.js').LinkedList;
 
-console.log(ll);
+// console.log(ll);
+// ll.addToHead(100);
+// console.log(ll);
+// ll.addToHead(200);
+// console.log(ll);
+// ll.addToHead(300);
+// console.log(ll);
+// debugger;
+
+// ll.addToTail(50);
+// debugger;
+
+ll.addToTail(10);
+ll.addToTail(20);
+ll.addToTail(30);
 ll.addToHead(100);
-console.log(ll);
-ll.addToHead(200);
-console.log(ll);
-ll.addToHead(300);
-console.log(ll);
-debugger;
 
-ll.addToTail(50);
-debugger;
+console.log(ll)
 
+console.log(ll.removeHead());
+console.log(ll.removeTail());
+
+
+debugger;
--- linkedlist.js
+++ linkedlist.js
@@ -11,8 +11,10 @@ function Node(value, next, prev) {
 
 LinkedList.prototype.addToHead = function (value) {
     var newNode = new Node(value, this.head, null);
+    //If there is already data in list slide head over and take it's place
     if (this.head) {
         this.head.prev = newNode;
+    //this is the first node in the list so set tail to this node also
     } else {
         this.tail = newNode;
     }
@@ -21,12 +23,48 @@ LinkedList.prototype.addToHead = function (value) {
 
 LinkedList.prototype.addToTail = function (value) {
     var newNode = new Node(value, null, this.tail);
+    //If there is already data in list slide tail over and take it's place
     if (this.tail) {
         this.tail.next = newNode;
+    //this is the first node in the list so set head to this node also
     } else {
         this.head = newNode;
     }
     this.tail = newNode;
+}
+
+LinkedList.prototype.removeHead = function () {
+    //if list is empty get outa here
+    if (!this.head) { return null; }
+
+    var val = this.head.value;
+    this.head = this.head.next;
+    //if the list is not empty set value of prev node null (popping this node off)
+    if (this.head) {
+        this.head.prev = null;
+    //list is now empty so set tail to null
+    } else {
+        this.tail = null;
+    }
+
+    return val;
+}
+
+LinkedList.prototype.removeTail = function () {
+    //if list is empty get outa here
+    if (!this.tail) { return null; }
+
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    //if the list is not empty set value of next node
+    if (this.tail) {
+        this.tail.next = null;
+    //list is now empty so set head to null
+    } else {
+        this.head = null;
+    }
+
+    return val;
 }
 
 exports.LinkedList = new LinkedList();
