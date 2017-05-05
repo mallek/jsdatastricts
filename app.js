db86577be8380a14e8f9255e0cff8f9a5850c7f0
Travis Haley <thaley@dlr360.com>
2017-05-05 10:30:58 -0600

    Init

---
+++ app.js
@@ -0,0 +1,14 @@
+var ll = require('./linkedlist.js').LinkedList;
+
+console.log(ll);
+ll.addToHead(100);
+console.log(ll);
+ll.addToHead(200);
+console.log(ll);
+ll.addToHead(300);
+console.log(ll);
+debugger;
+
+ll.addToTail(50);
+debugger;
+
